ee30e04fb9a40a96e79a4635b32fac369210157f
ChangHu <66352520+changhu73@users.noreply.github.com>
2024-07-31 16:28:58 +0800

    completed

--- social_mining/test.py
+++ social_mining/test.py
@@ -1,41 +1,49 @@
-# read npz file
-import scipy.sparse as sp
+# # read weights.npz(original E)
+# import scipy.sparse as sp
+# import pandas as pd
 
-def read_sparse_npz_file(file_path):
-    try:
-        sparse_matrix = sp.load_npz(file_path)
-        return sparse_matrix
-    except Exception as e:
-        print(f"读取npz文件时出错: {e}")
-        return None
-
-file_path = 'c:/Users/13772/OneDrive/文档/GitHub/electrolyte_papers_extraction/social_mining/weights.npz'
-
-sparse_matrix = read_sparse_npz_file(file_path)
-if sparse_matrix is not None:
-    print("稀疏矩阵:")
-    print(sparse_matrix)
-    print("稀疏矩阵转为密集矩阵:")
-    print(sparse_matrix.toarray())
-
-
-
-
-
-# # read pkl file
-# import pickle
-
-# def read_pkl_file(file_path):
+# def read_sparse_npz_file(file_path):
 #     try:
-#         with open(file_path, 'rb') as file:
-#             data = pickle.load(file)
-#         return data
+#         sparse_matrix = sp.load_npz(file_path)
+#         return sparse_matrix
 #     except Exception as e:
-#         print(f"读取pkl文件时出错: {e}")
+#         print(f"读取npz文件时出错: {e}")
 #         return None
 
-# file_path = 'c:/Users/13772/OneDrive/文档/GitHub/electrolyte_papers_extraction/social_mining/author_weights.pkl'
+# file_path = 'c:/Users/13772/OneDrive/文档/GitHub/electrolyte_papers_extraction/social_mining/weights.npz'
 
-# data = read_pkl_file(file_path)
-# if data is not None:
-#     print(data)
+# sparse_matrix = read_sparse_npz_file(file_path)
+# if sparse_matrix is not None:
+#     print("稀疏矩阵:")
+#     print(sparse_matrix)
+#     print("稀疏矩阵转为密集矩阵:")
+#     print(sparse_matrix.toarray())
+
+
+
+
+# # read total_citation.npz(using)
+# import scipy.sparse as sp
+# import pandas as pd
+# total_citation = pd.read_pickle('c:/Users/13772/OneDrive/文档/GitHub/electrolyte_papers_extraction/social_mining/total_citation.npz')
+# print(total_citation.columns)
+# print(total_citation.index)
+
+
+# read pkl file(V)
+import pickle
+
+def read_pkl_file(file_path):
+    try:
+        with open(file_path, 'rb') as file:
+            data = pickle.load(file)
+        return data
+    except Exception as e:
+        print(f"读取pkl文件时出错: {e}")
+        return None
+
+file_path = 'c:/Users/13772/OneDrive/文档/GitHub/electrolyte_papers_extraction/social_mining/authors.pkl'
+
+data = read_pkl_file(file_path)
+if data is not None:
+    print(data)
--- social_mining/view.py
+++ social_mining/view.py
@@ -1,52 +1,75 @@
+
 import pickle
 import numpy as np
 import networkx as nx
+import pandas as pd
 from pyvis.network import Network
 from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
 
 with open('c:/Users/13772/OneDrive/文档/GitHub/electrolyte_papers_extraction/social_mining/author_weights.pkl', 'rb') as f:
     data = pickle.load(f)
 
+with open('c:/Users/13772/OneDrive/文档/GitHub/electrolyte_papers_extraction/social_mining/authors.pkl', 'rb') as f:
+    element_id_to_author = pickle.load(f)
+
 G = nx.Graph()
-
-# 做element_ID到数字的映射
-id_list = list(data.keys())
-id_mapping = {id_: i for i, id_ in enumerate(id_list)}
-
 scaler = MinMaxScaler()
-weights = np.array([data[id_][0] for id_ in id_list]).reshape(-1, 1)
-scaled_weights = scaler.fit_transform(weights).flatten()
 
-for id_, scaled_weight in zip(id_list, scaled_weights):
-    G.add_node(id_mapping[id_], size=scaled_weight)
+weights = np.array([data[id_][0] for id_ in data.keys()]).reshape(-1, 1)
+scaled_weights = scaler.fit_transform(weights).flatten()
+min_node_size = 5
+
+for id_, scaled_weight in zip(data.keys(), scaled_weights):
+    node_size = max(scaled_weight * 500, min_node_size)
+    G.add_node(id_, value=node_size)
+
+total_citation = pd.read_pickle('c:/Users/13772/OneDrive/文档/GitHub/electrolyte_papers_extraction/social_mining/total_citation.npz')
+
+for row in total_citation.index:
+    for col in total_citation.columns:
+        if total_citation.at[row, col] > 0:
+            G.add_edge(row, col, weight=total_citation.at[row, col])
 
 net = Network(notebook=True)
 
 net.from_nx(G)
 
+connected_components = list(nx.connected_components(G))
+num_components = len(connected_components)
+
+colors = [plt.cm.Pastel1(i / num_components) for i in range(num_components)]
+colors = ['#%02x%02x%02x' % (int(r*255), int(g*255), int(b*255)) for r, g, b, _ in colors]
+
+component_color_map = {}
+for i, component in enumerate(connected_components):
+    color = colors[i]
+    for node in component:
+        component_color_map[node] = color
 
 for node in net.nodes:
     node_id = node['id']
-    node['size'] = G.nodes[node_id]['size'] * 200
-    node['label'] = str(node_id)
+    node['size'] = G.nodes[node_id]['value']  
+    node['label'] = element_id_to_author.get(node_id, str(node_id)) 
+    node['color'] = component_color_map[node_id] 
 
-# # testing for the nodes(too small)
-# for node in net.nodes:
-#     node_id = node['id']
-#     node['size'] = max(G.nodes[node_id]['size'] * 200, 10)
-#     node['label'] = str(node_id)
+uniform_width = 2
+for edge in net.edges:
+    edge['width'] = uniform_width
 
-# # Gamma校正(替换三个)
-# def gamma_correction(value, gamma=0.5):
-#     return value ** gamma
-
-# gamma_corrected_weights = np.array([gamma_correction(w) for w in scaled_weights])
-
-# for id_, gamma_corrected_weight in zip(id_list, gamma_corrected_weights):
-#     G.add_node(id_mapping[id_], size=gamma_corrected_weight)
-
+net.set_options("""
+var options = {
+  "physics": {
+    "barnesHut": {
+      "gravitationalConstant": -20,
+      "centralGravity": 0.3,
+      "springLength": 95,
+      "springConstant": 0.04,
+      "damping": 0.09,
+      "avoidOverlap": 0.5
+    }
+  }
+}
+""")
 
 net.show('author_weights_network.html')
-
-
-
